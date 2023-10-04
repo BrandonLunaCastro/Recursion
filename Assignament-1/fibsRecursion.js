857e76d4bc4be934aca97dc2a0626b711cd2637b
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-04 11:50:54 -0300

    pending revise

--- Assignament-1/fibsRecursion.js
+++ Assignament-1/fibsRecursion.js
@@ -1,12 +1,2 @@
-function fibsRecursive(num){
-    //console.log(num)
-    let arr = []
-    if(num === 1 || num === 0 ){
-        return num;
-    }
-    else{
-       return fibsRecursive(num - 1) + fibsRecursive(num - 2)
-    
-    }
-}
-console.log(fibsRecursive(7));
+//pending
+
